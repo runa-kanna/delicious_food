a0ed42059991e8f91bac573b209791d3e483b9b3
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-11-06 02:06:43 +0000

    [add]投稿機能・投稿一覧機能・投稿詳細機能・投稿削除機能

---
+++ db/migrate/20221105014824_create_foods.rb
@@ -0,0 +1,11 @@
+class CreateFoods < ActiveRecord::Migration[6.1]
+  def change
+    create_table :foods do |t|
+      
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
