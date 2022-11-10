18cab5d98bbc146d8122d73d0b75c40aa44349a4
AyakaIrie <112145166+runa-kanna@users.noreply.github.com>
2022-11-10 12:33:40 +0000

    [add]トップ.about.login.signupのレイアウト

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,4 +8,13 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to user_path(user), notice: 'guestuserでログインしました。'
   end
   
+  private
+
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 end
